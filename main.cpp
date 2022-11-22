172cec6f95673dc2ac8c5ed7e71c885aa4faab20
francescotavian <117820691+francescotavian@users.noreply.github.com>
2022-11-22 12:55:30 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,24 @@
 using namespace std;
 
 int main() {
+    int numero;
+    int primoNumero=1;
+    int secondoNumero=1;
+    cin>>numero;
+    if(numero>=2) {
+    cout<< primoNumero<<endl<<secondoNumero <<endl ;
+        int i = 3;
+        while (i++ <= numero) {
+            cout << primoNumero + secondoNumero <<endl;
+            if (primoNumero < secondoNumero)
+                primoNumero += secondoNumero;
+
+            else
+                secondoNumero += primoNumero;
+        }
+    }
+    else{
+        cout<<"errore";
+    }
    return 0;
 }
